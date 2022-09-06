e088817c3345aca5dc9e2bdbda706e83758851b2
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-06 22:51:09 +0530

    file opened for writing and wrote header row...

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import requests
 url = 'https://www.imdb.com/chart/top/'
 source = requests.get(url).text
 
+# file opening
+csv_file = open('imdb_data.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Movie Name', 'Release Year', 'Movie Rating'])
+
 soup = BeautifulSoup(source, 'lxml')
 title_col = soup.find_all('td', class_='titleColumn')
 rating_col = soup.find_all('td', class_='ratingColumn imdbRating')
