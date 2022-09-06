411a59e477065f3233db3be2b0f387bb9b798404
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-06 23:13:17 +0530

    file created and data wrote into the file

--- main.py
+++ main.py
@@ -13,3 +13,11 @@ csv_writer.writerow(['Movie Name', 'Release Year', 'Movie Rating'])
 soup = BeautifulSoup(source, 'lxml')
 title_col = soup.find_all('td', class_='titleColumn')
 rating_col = soup.find_all('td', class_='ratingColumn imdbRating')
+
+for full_title, full_rating in zip(title_col, rating_col):
+    title = full_title.a.text
+    year = full_title.span.text[1:-1]
+    rating = full_rating.strong.text
+    csv_writer.writerow([title, year, rating])
+
+csv_file.close()
